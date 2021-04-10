9ed86a284a270506c954affd26585f702d1145a2
Chris McBride <christophermc@zg06802.zillow.local>
2021-04-09 19:31:56 -0700

    Adds Ruby Item Mod

---
+++ src/main/java/net/fabricmc/example/RubyItemMod.java
@@ -0,0 +1,6 @@
+public class ExampleMod implements ModInitializer {
+ 
+    // an instance of our new item
+    public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    [...]
+}
